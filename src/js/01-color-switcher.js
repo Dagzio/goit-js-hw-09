58fd5a62f94eeb998c7fbc84a7fab01a4ded864a
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-03-14 11:26:43 +0200

    done task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+let startBtnId = null;
+
+stopBtn.setAttribute('disabled', '');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onStartBtnClick() {
+
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled');
+
+   startBtnId = setInterval(() => {
+        document.querySelector("body").style.backgroundColor = getRandomHexColor();
+   }, 1000);
+    
+};
+
+function onStopBtnClick() {
+
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', '');
+
+    clearInterval(startBtnId);
+}
+
 
