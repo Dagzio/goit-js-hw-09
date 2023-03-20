8b288b7246125ea0ceede3faf922763aa8913fb9
Valentine <strheadshot1997@gmail.com>
2023-03-20 12:40:13 +0200

    done task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,50 +1,41 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-const delayInput = document.querySelector('input[name="delay"');
-const step = document.querySelector('input[name="step"');
-const amountInput = document.querySelector('input[name="amount"');
-const submitBtn = document.querySelector('button[type="submit"]');
-
-submitBtn.addEventListener('submit', createPromise);
-
- const promise = new Promise((resolve, reject) => {
-   resolve = setInterval(createPromise, delayInput.value);
-   
-    
-  });
-
-
-
+const formEl = document.querySelector(".form");
+formEl.addEventListener('submit', onSubmitForm);
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-  if (shouldResolve) {
-     
-
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
     } else {
+        reject({ position, delay });
+    }
+    }, delay)
+    
+    
+  });
 
-    // Reject
-      
-  };
-  
-  return promise;
-  
-  
 };
 
 
-
-
-
-
-
-createPromise(3, 1500)
-  .then(({ position, delay }) => {
+function onSubmitForm(event) {
+  event.preventDefault();
+  let delayValue = Number(formEl.delay.value);
+  for (let i = 0; i <= formEl.amount.value; i += 1){
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
     Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     Notify.failure(`Rejected promise ${position} in ${delay}ms`);
   });
+    delayValue += Number(formEl.step.value);
+  }
+};
+
+
